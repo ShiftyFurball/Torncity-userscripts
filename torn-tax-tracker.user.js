fd0a19b1db401a85c6fabd835f28cb4644a1be85
ShiftyFurball <183384343+ShiftyFurball@users.noreply.github.com>
2025-10-16 19:16:48 +0100

    Bump script version to 6.7

--- torn-tax-tracker.user.js
+++ torn-tax-tracker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn Lingerie Store Tax Tracker
 // @namespace    http://tampermonkey.net/
-// @version      6.6
+// @version      6.7
 // @description  Track weekly company tax from employees in Torn with Torn-styled table, draggable/resizable panel, reminders, overpayment tracking, totals row, and Test Mode.
 // @author       Hooded_Prince
 // @match        https://www.torn.com/*
